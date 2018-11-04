65d2d2b13131b4c3f267bb4da428d986c1f0582b
leco <lcosta.sp.br@gmail.com>
2018-11-04 20:24:02 -0200

    initial project

---
+++ src/main/java/guru/leco/springbootjwt/SpringBootJwtApplication.java
@@ -0,0 +1,12 @@
+package guru.leco.springbootjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtApplication.class, args);
+	}
+}
